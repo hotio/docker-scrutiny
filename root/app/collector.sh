2894a4be8e424720b978a3b57ec806839f00c96d
hotio <26902309+hotio@users.noreply.github.com>
2020-09-20 00:28:33 +0200

    add startup delay

--- root/app/collector.sh
+++ root/app/collector.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+currenttime=$(date +%s); maxtime=$((currenttime+60)); while (! curl -fsSL "${2}" > /dev/null) && [[ "$currenttime" -lt "$maxtime" ]]; do sleep 1; currenttime=$(date +%s); done
+
 while true; do
     DEBUG=true "${APP_DIR}/scrutiny-collector-metrics" run --api-endpoint "${2}"
     sleep "${1}"
