8f271fbd2115ab2d71d244f0b7ed15c3a0d89949
hotio <26902309+hotio@users.noreply.github.com>
2020-09-20 00:38:06 +0200

    be silent

--- root/app/collector.sh
+++ root/app/collector.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-currenttime=$(date +%s); maxtime=$((currenttime+60)); while (! curl -fsSL "${2}" > /dev/null) && [[ "$currenttime" -lt "$maxtime" ]]; do sleep 1; currenttime=$(date +%s); done
+currenttime=$(date +%s); maxtime=$((currenttime+60)); while (! curl -fsSL "${2}" > /dev/null 2>&1) && [[ "$currenttime" -lt "$maxtime" ]]; do sleep 1; currenttime=$(date +%s); done
 
 while true; do
     DEBUG=true "${APP_DIR}/scrutiny-collector-metrics" run --api-endpoint "${2}"
