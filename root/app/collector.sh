13db3188bfbdd7557c02c0d1215b2cff0e31a295
hotio <26902309+hotio@users.noreply.github.com>
2020-09-22 09:12:53 +0200

    fix startup

--- root/app/collector.sh
+++ root/app/collector.sh
@@ -2,7 +2,9 @@
 
 currenttime=$(date +%s); maxtime=$((currenttime+60)); while (! curl -fsSL "${2}" > /dev/null 2>&1) && [[ "$currenttime" -lt "$maxtime" ]]; do sleep 1; currenttime=$(date +%s); done
 
+[[ ${3} == "--debug" ]] && DEBUG="--debug"
+
 while true; do
-    DEBUG=true "${APP_DIR}/scrutiny-collector-metrics" run --api-endpoint "${2}"
+    "${APP_DIR}/scrutiny-collector-metrics" run --api-endpoint "${DEBUG}"
     sleep "${1}"
 done
