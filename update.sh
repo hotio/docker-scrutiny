856a39d9e7667f2a64f4248e3089ac295194a029
mrhotio <26902309+mrhotio@users.noreply.github.com>
2021-02-13 13:08:05 +0100

    skip ci

--- update.sh
+++ update.sh
@@ -14,7 +14,7 @@ elif [[ ${1} == "tests" ]]; then
     docker run --rm --entrypoint="" "${2}" apk -vv info | sort
     echo "Check if app works..."
     app_url="http://localhost:8080"
-    docker run --rm --network host -d --name service --privileged -v /run/udev:/run/udev:ro -v /dev/disk:/dev/disk:ro "${2}"
+    docker run --network host -d --name service --privileged -v /run/udev:/run/udev:ro -v /dev/disk:/dev/disk:ro "${2}"
     currenttime=$(date +%s); maxtime=$((currenttime+60)); while (! curl -fsSL "${app_url}" > /dev/null) && [[ "$currenttime" -lt "$maxtime" ]]; do sleep 1; currenttime=$(date +%s); done
     curl -fsSL "${app_url}" > /dev/null
     status=$?
